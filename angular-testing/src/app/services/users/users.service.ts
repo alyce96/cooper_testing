6c2387fa981204ae7185bbac7b9e73fe3f111dab
Alyce Cooper <alycoo1256@students.ecpi.edu>
2024-08-25 13:52:25 -0400

    //adding files

---
+++ angular-testing/src/app/services/users/users.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs'; // Add imports
+
+export type User = {
+  id: string;
+  name: string;
+  role: string;
+  pokemon: string;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class UsersService {
+  users: Array<User> = [ // Add employee object
+  {
+    id: '1',
+    name: 'Jane',
+    role: 'Designer',
+    pokemon: 'Blastoise'
+  },
+  {
+    id: '2',
+    name: 'Bob',
+    role: 'Developer',
+    pokemon: 'Charizard'
+  },
+  {
+    id: '3',
+    name: 'Jim',
+    role: 'Developer',
+    pokemon: 'Venusaur'
+  },
+  {
+    id: '4',
+    name: 'Adam',
+    role: 'Designer',
+    pokemon: 'Yoshi'
+  }
+];
+
+  constructor() { }
+
+  all(): Observable<Array<User>> {
+    return of(this.users);
+  }
+}
